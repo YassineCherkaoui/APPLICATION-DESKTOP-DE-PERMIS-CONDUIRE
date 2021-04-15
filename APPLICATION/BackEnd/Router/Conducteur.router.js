4d02e2b9c769d1ab04013142069554ecc8d33d2c
YassineCherkaoui <yassin.cherkaoui2000@gmail.com>
2021-04-15 13:57:46 +0000

    V1

---
+++ APPLICATION/BackEnd/Router/Conducteur.router.js
@@ -0,0 +1,20 @@
+module.exports = function (app) {
+
+
+
+    // --------------------Calling Controller File----------------- 
+    var Conducteur = require('../Controllers/Conducteur');
+  
+
+    
+    //------------------------get all Catigories---------------------
+    app.post('/Conducteur/Add', Conducteur.ConducteurAdd);
+    //------------------------get all Catigories---------------------
+    app.get(`/validateAccount/:token`, Conducteur.conducteurActivated);
+
+    
+    //------------------------get all Conducteur---------------------
+    app.get('/Conducteur', Conducteur.getConducteur);
+
+
+}
